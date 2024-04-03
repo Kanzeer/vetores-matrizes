d034456453fdb18acaebe7bbf15c5e086b0c0707
Kanzeer <miguelroaamaral@gmail.com>
2024-04-03 16:27:31 -0400

    questão 5 finalizada

---
+++ questao5.js
@@ -0,0 +1,9 @@
+//Questão 5: Crie uma matriz 3x3 e imprima o elemento na terceira linha e segunda coluna em JavaScript?
+
+let matriz = [ //Criando uma matriz de arrays
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+console.log(matriz[1][2]); //saída: 6
+console.log(matriz[2][0]); //saída: 7
