09ec4d4513d02f735f07fbff919d007f3a082729
Kanzeer <miguelroaamaral@gmail.com>
2024-04-03 16:27:10 -0400

    questão 2 finalizada

---
+++ questao2.js
@@ -0,0 +1,7 @@
+//Questão 2: Crie um código em JavaScript para adicionar um elemento no final de um vetor
+
+let array = [1, 2, 3, 4, 5];
+console.log(array); //Código primário
+
+array.push(6); //Adicionando o valor "6" ao código primário
+console.log(array);
